9933f22c0dceac64c1d01bad149f209b1fe6e9ce
gistop <gistop@163.com>
2026-06-23 10:04:47 +0800

    Publish demo-html-site: script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const form = document.querySelector(".contact-form");
 if (form) {
   form.addEventListener("submit", (event) => {
     event.preventDefault();
-    alert("咨询已提交，我们会尽快联系你。");
+    alert("感谢咨询，我们会尽快联系你。");
   });
 }
